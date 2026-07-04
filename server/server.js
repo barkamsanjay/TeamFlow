4ce3b4f6ff17fc490d20301d5c4c22e4b67ab02e
sanjay <barkamsanjay1@gmail.com>
2026-07-04 14:45:44 +0530

    Setup MongoDB Atlas connection

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("TeamFlow API Running");
+  res.send("🚀 TeamFlow API Running");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
