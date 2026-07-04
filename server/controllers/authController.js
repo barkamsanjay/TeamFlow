7ef258f093c2d5e1c6d56cd662651af95e7485f8
sanjay <barkamsanjay1@gmail.com>
2026-07-04 15:43:25 +0530

    implemented user registration API

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,46 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
 export const registerUser = async (req, res) => {
-  res.status(200).json({
-    message: "Register API Working",
-  });
+  try {
+    const { name, email, password } = req.body;
+
+    // Step 1: Check all fields
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "Please fill all fields",
+      });
+    }
+
+    // Step 2: Check existing user
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // Step 3: Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Step 4: Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User Registered Successfully",
+      user,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
 };
